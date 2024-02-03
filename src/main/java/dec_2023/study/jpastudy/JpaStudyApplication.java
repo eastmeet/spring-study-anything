170c66d368ea6fdf71b3f5e71fa2cdbe4b643855
eastmeet <ys932184@gmail.com>
2024-02-04 00:45:33 +0900

    feature: 초난감 UserDao 작성 및 테스트

--- src/main/java/dec_2023/study/jpastudy/JpaStudyApplication.java
+++ src/main/java/dec_2023/study/jpastudy/JpaStudyApplication.java
@@ -1,13 +1,42 @@
 package dec_2023.study.jpastudy;
 
+import dec_2023.study.jpastudy.user.dao.UserDao;
+import dec_2023.study.jpastudy.user.entity.User;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class JpaStudyApplication {
+import java.sql.SQLException;
 
-    public static void main(String[] args) {
+@SpringBootApplication
+public class JpaStudyApplication implements CommandLineRunner {
+
+    private final UserDao dao;
+
+    public JpaStudyApplication(UserDao userDao) {
+        this.dao = userDao;
+    }
+
+    public static void main(String[] args) throws SQLException, ClassNotFoundException {
         SpringApplication.run(JpaStudyApplication.class, args);
     }
 
+
+    @Override
+    public void run(String... args) throws Exception {
+
+        User user = new User();
+        user.setId("eastmeet");
+        user.setName("이동우");
+        user.setPassword("hi");
+
+        dao.add(user);
+
+        System.out.println(user.getId() + "등록 성공");
+
+        User user2 = dao.get(user.getId());
+        System.out.println(user2.getName());
+        System.out.println(user2.getPassword());
+        System.out.println(user2.getId() + "조회 성공");
+    }
 }
---
+++ src/main/java/dec_2023/study/jpastudy/user/dao/UserDao.java
@@ -0,0 +1,63 @@
+package dec_2023.study.jpastudy.user.dao;
+
+import dec_2023.study.jpastudy.user.entity.User;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import java.sql.*;
+
+@Component
+public class UserDao {
+
+    @Value("${datasource.driver-class-name}")
+    private String className;
+
+    @Value("${datasource.url}")
+    private String url;
+
+    @Value("${datasource.username}")
+    private String dbUserName;
+
+    @Value("${datasource.password}")
+    private String dbPassword;
+
+    public void add(User user) throws ClassNotFoundException, SQLException {
+        Class.forName(className);
+        Connection c = DriverManager.getConnection(url, dbUserName, dbPassword);
+
+        PreparedStatement ps = c.prepareStatement("insert into users(id, name, password) values(?,?,?)");
+
+        ps.setString(1, user.getId());
+        ps.setString(2, user.getName());
+        ps.setString(3, user.getPassword());
+
+        ps.executeUpdate();
+
+        ps.close();
+        c.close();
+    }
+
+    public User get(String id) throws ClassNotFoundException, SQLException {
+        Class.forName(className);
+        Connection c = DriverManager.getConnection(url, dbUserName, dbPassword);
+
+
+        PreparedStatement ps = c.prepareStatement("select * from users where id = ?");
+        ps.setString(1, id);
+
+        ResultSet rs = ps.executeQuery();
+        rs.next();
+        User user = new User();
+        user.setId(rs.getString("id"));
+        user.setName(rs.getString("name"));
+        user.setPassword(rs.getString("password"));
+
+        rs.close();
+        ps.close();
+        c.close();
+
+        return user;
+    }
+
+
+}
