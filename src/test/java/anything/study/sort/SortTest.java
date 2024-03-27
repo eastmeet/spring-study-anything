199abe1287c80e2ad66bae3522ace39f3c034cf1
eastmeet <ys932184@gmail.com>
2024-03-27 21:26:23 +0900

    feature: selection sort 구현

--- src/test/java/anything/study/sort/SortTest.java
+++ src/test/java/anything/study/sort/SortTest.java
@@ -1,22 +1,25 @@
 package anything.study.sort;
 
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
-
-import java.util.Arrays;
 
 class SortTest {
 
+    int[] array = new int[]{5, -2, 3, 25, 21, -55, 27, -30};
+
     @Test
+    @Order(1)
+    @DisplayName("버블정렬 O(n^2), Stable, Swapping neighbors")
     void bubbleSort() {
 
-        int[] array = new int[]{5, -2, 3, 25, 21, -55, 27, -30};
-
         boolean swapped;
-        for (int i = array.length - 1; i > 0; i--) {
+        for (int lastUnsortedIndex = array.length - 1; lastUnsortedIndex > 0; lastUnsortedIndex--) {
             swapped = false;
-            for (int j = 0; j < i; j++) {
-                if (array[j] > array[j + 1]) {
-                    swap(array, j);
+            for (int i = 0; i < lastUnsortedIndex; i++) {
+                if (array[i] > array[i + 1]) {
+                    swap(array, i, i + 1);
                     swapped = true;
                 }
             }
@@ -26,14 +29,35 @@ class SortTest {
             }
         }
 
-        System.out.println("Arrays.toString(array) = " + Arrays.toString(array));
+        Assertions.assertThat(array).isSorted();
     }
 
-    private static void swap(int[] array, int j) {
-        int temp = array[j];
-        array[j] = array[j + 1];
-        array[j + 1] = temp;
+    @Test
+    @Order(2)
+    @DisplayName("선택정렬 O(n^2), Unstable, Identify-and-place")
+    void selectionSort() {
+
+        for (int lastUnsortedIndex = array.length - 1; lastUnsortedIndex > 0; lastUnsortedIndex--) {
+
+            int largest = 0;
+
+            for (int i = 1; i <= lastUnsortedIndex; i++) {
+                if (array[i] > array[largest]) {
+                    largest = i;
+                }
+            }
+
+            swap(array, largest, lastUnsortedIndex);
+
+        }
+
+        Assertions.assertThat(array).isSorted();
     }
 
+    private static void swap(int[] array, int i, int j) {
+        int temp = array[i];
+        array[i] = array[j];
+        array[j] = temp;
+    }
 
 }
